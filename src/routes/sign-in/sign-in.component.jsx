6580d5364cd7adac907e61d4c99809a90ada9112
Anthony Suarez <antsuarez305@gmail.com>
2022-06-14 17:33:07 -0500

    Sign in redirect page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,19 +2,28 @@ import React from 'react'
 
 import {
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 
 function SignIn() {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
-  const userDocRef = await createUserDocumentFromAuth(user)
+    const userDocRef = await createUserDocumentFromAuth(user)
+  }
+
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect()
+    console.log({ user })
   }
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign in with Google Redirect
+      </button>
     </div>
   )
 }
